a0e45abc99711e38b9274d9e38f334628c07d05e
dmegaritis <dmgrei3@gmail.com>
2024-09-23 14:05:54 +0100

    predictions added for data

--- examples/load_model.py
+++ examples/load_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os
 try:
     from tensorflow.keras.models import load_model
     print("Import successful!")
@@ -9,3 +8,12 @@ except ImportError as e:
 # Load the model
 model_path = '../pre_trained_models/model_D.h5'
 model = load_model(model_path)
+
+# Load the example data
+example_data_path = '../examples/example_data.npy'
+data = np.load(example_data_path)
+
+# Make predictions
+predictions = model.predict(data)
+predicted_classes = (predictions > 0.5).astype(int)
+print("Predictions:", predicted_classes)
